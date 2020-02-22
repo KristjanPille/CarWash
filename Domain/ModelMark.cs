5db888bdf6ccda71972d9ed1ac80492298d60d9b
krist <krpill@ttu.ee>
2020-02-22 10:21:22 +0200

    initial push

--- Domain/ModelMark.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain
-{
-    public class ModelMark
-    {
-        public int ModelMarkID { get; set; }
-        public string mark { get; set; }
-        public string model { get; set; }
-    }
-}

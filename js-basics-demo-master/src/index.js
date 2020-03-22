18876caf12236c8de83530e4e89c35bd33ccd77d
krpill <krpill@ttu.ee>
2020-03-22 15:22:51 +0200

    Push

--- js-basics-demo-master/src/index.js
+++ js-basics-demo-master/src/index.js
@@ -4,3 +4,6 @@ import { handleCellClick, handleRestartGame, computerVsHuman } from "./engine.js
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
 document.querySelector('.game--computer').addEventListener('click', computerVsHuman);
+
+
+

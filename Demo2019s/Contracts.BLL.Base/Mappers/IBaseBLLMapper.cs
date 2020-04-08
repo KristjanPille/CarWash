9fceacb1753bdc02772f91b1a9c104ea1eec5c04
krpill <krpill@ttu.ee>
2020-04-08 09:29:09 +0300

    das

---
+++ Demo2019s/Contracts.BLL.Base/Mappers/IBaseBLLMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts.BLL.Base.Mappers
+{
+    public interface IBaseBLLMapper
+    {
+        TOutObject Map<TOutObject, TInObject>(object inObject)
+            where TOutObject : class, new()
+            where TInObject : class, new ();
+    }
+}
---
+++ Demo2019s/Contracts.BLL.Base/Services/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Contracts.DAL.Base;
+using Contracts.DAL.Base.Repositories;
+
+namespace Contracts.BLL.Base.Services
+{
+    public interface IBaseService
+    {
+        
+    }
+    public interface IEntityService<TBLLEntity> : IBaseService, IBaseRepository<TBLLEntity>
+        where TBLLEntity : class, IDomainEntity<Guid>, new()
+    {
+        
+    }
+}
